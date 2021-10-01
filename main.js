d0c07b1ab279fa23abca7f0eff4d0b38c8d83b3c
MeenuMe <73883057+MeenuMe@users.noreply.github.com>
2021-10-01 16:48:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function preload(){
+
+}
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,500);
+    canvas.position(560,150);
+    posenet = ml5.poseNet(video,modelLoaded);
+    posenet.on('pose', gotPoses);
+
+}
+
+function draw(){
+    background(0);
+}
+
+function modelLoaded(){
+    console.log("The model  is loaded as usual so dont worry");
+}
+
+function gotPoses(results){
+    if (results.length>0){
+        console.log(results);
+    }
+}
